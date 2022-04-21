b3193f700e4bbf394a995274c07da17210c12001
Eric Martinet <93914292+eric-martinet@users.noreply.github.com>
2022-04-21 13:01:12 +0200

    Update ParisSubsidies.py

--- 07_Streamlit/ParisSubsidies.py
+++ 07_Streamlit/ParisSubsidies.py
@@ -138,19 +138,71 @@ with col3:
     st.metric(label = 'Avg reject rate', value = f'{avg_reject_rate:.1%}')
     st.caption('Almost 4 out of 10 requests are rejected: what is the pattern?')
 
-# col1, col2 = st.columns(2)
-# with col1:
-#     st.metric(label = 'Max subsidy', value = format_number(max_subsidy)+'€')
-#     st.caption('Granted to "Théâtre Musical de Paris" in 2013 as a "Subvention d\'équilibre".')
-# with col2:
-#     st.metric(label = 'Min subsidy', value = format_number(min_project_subsidy)+'€')
-#     st.caption('Granted to "Assocation Osadhi" in 2019 for the "Printemps des Cimetières".')
+
 
 # ...under the hood...
 with st.expander('Under the hood'):
     st.markdown('''
     In total, it is more than 2.4B€ worth of subsidies that we have analysed over 2013-2021, granted to about 6,800 different associations.
     ''')
+    col1, col2 = st.columns(2)
+    with col1:
+        st.metric(label = 'Max subsidy', value = format_number(max_subsidy)+'€')
+        st.caption('Granted to "Théâtre Musical de Paris" in 2013 as a "Subvention d\'équilibre".')
+    with col2:
+        st.metric(label = 'Min subsidy', value = format_number(min_project_subsidy)+'€')
+        st.caption('Granted to "Assocation Osadhi" in 2019 for the "Printemps des Cimetières".')
+
+# Distribution of subsidies
+st.subheader('Distribution analysis')
+st.write('''The Budget year is the reference period for public accounting. 
+By law, each spending must be approved by Paris City Council and allocated to a specific Budget year.
+''')
+
+@st.cache
+def distribution_analysis():
+    # Create figure with secondary y-axis
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    # Dataframes
+    gb = data.groupby('montant_vote_cat').agg(nb_dossiers = ('numero_dossier', 'count'), total_subsidies = ('montant_vote', 'sum'))
+    gb['avg_yearly_subsidies'] = gb.total_subsidies / nb_years
+    gb['avg_nb_dossiers'] = gb.nb_dossiers / nb_years
+
+    # Add traces
+    fig.add_trace(
+        go.Bar(x=gb.index, y=gb.avg_yearly_subsidies, name='Yearly subsidies'),
+        secondary_y=False,
+    )
+
+    fig.add_trace(
+        go.Scatter(x=gb.index, y=gb.avg_nb_dossiers, name='Number of requests'),
+        secondary_y=True,
+    )
+
+    # Layout
+    fig.update_layout(
+        title_text='<b>Yearly subsidies and Number of Requests by Subsidy size</b>',
+        showlegend = False,
+        hovermode='x unified',
+    )
+
+    # Set x-axis title
+    fig.update_xaxes(title_text='<b>Subsidy</b> size')
+
+    # Set y-axes titles
+    fig.update_yaxes(title_text='<b>Yearly</b> subsidies', color = '#636EFA', secondary_y=False)
+    fig.update_yaxes(title_text='<b>Number</b> of requests', color='#EF553B', showgrid = False, tickformat = '.0f', secondary_y=True)
+
+    return fig
+
+fig = distribution_analysis()
+st.plotly_chart(fig)
+st.caption('''
+Paris City Council was less and less selective from 2015 to 2018, and still below its medium-term average (39%) in 2019 and 2020.
+
+It never received so many requests as in 2021, but the reject rate soared: we can assume this is again the effect of the financial crisis induced by the covid-19 crisis.
+''')
 
 
 # Analysis by Budget year
@@ -181,7 +233,7 @@ def total_mean_subsidy_by_year():
 
     # Layout
     fig.update_layout(
-        title_text='<b>Total subsidies & Mean subsidy by Budget year (in EUR)</b>',
+        title_text='<b>Total subsidies & Mean subsidy by Budget year</b>',
         showlegend = False,
         hovermode='x unified',
     )
@@ -283,16 +335,116 @@ def subsidies_by_direction():
 
     # Layout
     fig.update_layout(
-        title_text='<b>Avg share in yearly subsidies by Direction</b>',
+        title_text='<b>Avg share in Yearly Subsidies by Direction</b>',
         showlegend = False
     )
 
     return fig
+
 
 fig = subsidies_by_direction()
 st.plotly_chart(fig)
 st.caption('''
 The top 2 Directions (DAC = Direction des Affaires Culturelles, DFPE = Direction des Familles et de la Petite Enfance) account for more than 50% of the granted subsidies.
 
-Adding DAE (Direction de l'Attractivité et de l'Emploi), DASES (Direction de l'Action Sociale de l'Enfance et de la Santé), and DRH (Direction des Ressources Humaines), we get to more than 80%.
+Adding the DAE (Direction de l'Attractivité et de l'Emploi), the DASES (Direction de l'Action Sociale de l'Enfance et de la Santé), and the DRH (Direction des Ressources Humaines), we get to more than 80% of the total.
+''')
+
+# Requests by Direction
+@st.cache
+def nb_requests_by_direction():
+    #Dataframes
+    gb = data.groupby('direction').agg(nb_requests = ('numero_dossier', 'count')).sort_values(by='nb_requests', ascending = False)
+    gb['avg_yearly_nb_requests'] = gb.nb_requests / nb_years
+    gb['avg_yearly_nb_requests'] = gb['avg_yearly_nb_requests'].astype(int)
+    gb = gb.drop('nb_requests', axis = 1)
+
+    # Group small
+    dir_ind = ['DAC', 'DFPE', 'DAE', 'DASES', 'DRH']
+    gb_others = {'Others':0}
+    lst_others = []
+
+    for dir in gb.index:
+        if dir not in dir_ind:
+            gb_others['Others'] += gb.loc[dir, 'avg_yearly_nb_requests']
+            lst_others.append(dir)
+
+    gb=pd.concat([gb.drop(lst_others, axis = 0), pd.DataFrame.from_dict(gb_others, orient='index', columns=['avg_yearly_nb_requests'])])
+    
+    # Sort as previous chart
+    rule = {
+        'DAC': 1,
+        'DFPE': 2,
+        'DAE': 3,
+        'DASES': 4,
+        'DRH':5,
+    }
+    gb.reset_index(level = 0, drop = False, inplace=True)
+    gb.sort_values(by='index', key=lambda x: x.apply(lambda y: rule.get(y, 1000)), inplace = True)
+    gb.set_index('index', inplace = True)
+
+    # Pie chart
+    fig = go.Figure(data=[go.Pie(labels=gb.index, values=gb.avg_yearly_nb_requests, textinfo='label+percent', sort = False, pull=[0,0,0,0,0,0.1])])
+
+    # Layout
+    fig.update_layout(
+        title_text='<b>Avg share in Yearly Requests by Direction</b>',
+        showlegend = False,
+
+    )
+
+    return fig
+
+fig = nb_requests_by_direction()
+st.plotly_chart(fig)
+st.caption('''
+However these 5 directions do not account for even half of the requests!
+
+For instance, the DRH only handles subsidies for Paris City's employees two representative bodies (Action Sociale des Personnels de la Ville de Paris with 4 requests per year, and AGOSPAP - APHP with 2 requests per year).
+''')
+
+@st.cache
+def nb_requests_reject_rate_by_direction():
+    # Create figure with secondary y-axis
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    # Dataframes
+    gb = data.groupby('direction').agg(avg_nb_requests = ('numero_dossier', 'count')).sort_values(by='avg_nb_requests', ascending = False)
+    gb.avg_nb_requests = np.ceil(gb.avg_nb_requests / nb_years)
+    ct = pd.crosstab(data.direction, data.subsidy_granted_bool, normalize='index').reindex(gb.index)
+
+
+    # Add traces
+    fig.add_trace(
+        go.Bar(x=gb.index, y=gb.avg_nb_requests, name='Number of requests'),
+        secondary_y=False,
+    )
+
+    fig.add_trace(
+        go.Scatter(x=ct.index, y=ct[False], name='Reject rate'),
+        secondary_y=True,
+    )
+
+    # Layout
+    fig.update_layout(
+        title_text='<b>Number of requests (yearly average) & Reject rate by Direction</b>',
+        showlegend = False,
+        hovermode='x unified',
+    )
+
+    # Set x-axis title
+    fig.update_xaxes(title_text='<b>Direction</b>')
+
+    # Set y-axes titles
+    fig.update_yaxes(title_text='<b>Number</b> of requests <i>(by year)</i>', color = '#636EFA', secondary_y=False)
+    fig.update_yaxes(title_text='<b>Reject</b> rate <i>(average)</i>', color='#EF553B', showgrid = False, range=[0,0.8],tickformat = '.0%', secondary_y=True)
+
+    return fig
+
+fig = nb_requests_reject_rate_by_direction()
+st.plotly_chart(fig)
+st.caption('''
+The DDCT (Direction de la Démocratie, des Citoyen.ne.s et des Territoires) is the Direction that processes the most requests. They distribute about 10M€ per year.
+
+Another important Direction we have not seen before is the DJS (Direction de la Jeunesse et des Sports). They distribute about 11M€ per year.
 ''')
